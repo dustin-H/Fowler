d40e7f63ffdbe501c466a3cfb9fe2d4533742c5d
dustin-H <dustin.hoffner@gmail.com>
2015-04-20 10:26:44 +0200

    Move method amountFor for better OOP

--- src/Rental.java
+++ src/Rental.java
@@ -1,14 +1,37 @@
 class Rental {
     private Movie movie;
     private int daysRented;
+
     public Rental(Movie newmovie, int newdaysRented) {
         movie = newmovie;
         daysRented = newdaysRented;
     }
+
     public int getDaysRented() {
         return daysRented;
     }
+
     public Movie getMovie() {
         return movie;
+    }
+
+    public double getCharge() {
+        double ret = 0;
+        switch (getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                ret += 2;
+                if (getDaysRented() > 2)
+                    ret += (getDaysRented() - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                ret += getDaysRented() * 3;
+                break;
+            case Movie.CHILDRENS:
+                ret += 1.5;
+                if (getDaysRented() > 3)
+                    ret += (getDaysRented() - 3) * 1.5;
+                break;
+        }
+        return ret;
     }
 }
